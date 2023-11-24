34d0bacd42208cd4db538db938737174b3b19fdd
nongparanai <promlerd.p@gmail.com>
2023-11-24 13:41:02 +0700

    feat(exercise) = add completed exercises

--- ex-3.js
+++ ex-3.js
@@ -1,9 +1,10 @@
 //Exercise #3: Debugging (Boolean & Logical Operations)
 
 //Start coding here
-const numberA = 100;
-const numberB = "20";
+let numberA = 100; // change declare form const to let
+let numberB = "20"; // change declare form const to let
 
 numberA = 10;
+numberB = Number(numberB); //convert numberB from String to Number
 
 console.log(`Sum: ${numberA + numberB}`);
